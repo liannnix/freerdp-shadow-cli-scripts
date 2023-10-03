cfc7dfb6280f61d4fb51157289249018afa06d09
Andrey Limachko <liannnix@altlinux.org>
2023-10-03 22:55:11 +0400

    initial commit

---
+++ remote-disconnect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+displays=
+
+function get_displays() {
+	while read -r i; do
+		grep " connected" <<< $i | cut -d " " -f 1
+	done <<< $(xrandr -q)
+}
+
+for i in $(get_displays); do
+	xrandr --output $i --auto
+done
